90f9fef868d9670d1f95ba46695cf2b00aceb0e9
bm1611 <bm1611@naver.com>
2026-06-12 14:02:46 +0900

    health 엔드포인트 추가(모델 체크)

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 import controller
+from service import profanityService
 
 app = FastAPI()
+
+
+@app.get('/health')
+async def health():
+    return {
+        'status': 'ok',
+        'modelLoaded': profanityService.isModelLoaded()
+    }
 
 if __name__ == '__main__':
     uvicorn.run('app:app', host='127.0.0.1', port=8001, reload=True)
