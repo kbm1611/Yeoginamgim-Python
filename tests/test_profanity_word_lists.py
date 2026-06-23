7896d6c27dac36f1734a6a30c11cd63083a8040f
bm1611 <bm1611@naver.com>
2026-06-23 15:46:54 +0900

    화이트리스트업데이트

--- tests/test_profanity_word_lists.py
+++ tests/test_profanity_word_lists.py
@@ -5,6 +5,67 @@ from pathlib import Path
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
+EXPECTED_ALLOW_EXACT_WORDS = {
+    "사발",
+    "사발면",
+    "물사발",
+    "밥사발",
+    "국사발",
+    "대접사발",
+    "사발통문",
+    "시발점",
+    "시발역",
+    "시발지",
+    "시발차",
+    "씨앗",
+    "씨알",
+    "씨눈",
+    "병식",
+    "병아리",
+    "병어",
+    "병풍",
+    "병충해",
+    "병정",
+    "지라",
+    "지리",
+    "개나리",
+    "개념",
+    "개구쟁이",
+    "개불",
+    "개살구",
+    "개암",
+    "개망초",
+    "개복치",
+    "개비",
+    "만년",
+    "닭살",
+    "닭장",
+    "망태기",
+    "멍울",
+    "변기",
+    "변소",
+    "새끼줄",
+    "십오",
+    "십육",
+    "시바견",
+    "자지러지다",
+    "자지러짐",
+    "젖병",
+    "젖니",
+    "젖먹이",
+    "젖산",
+    "젓갈",
+    "조개",
+    "토막",
+    "항문",
+    "치질",
+    "대장항문",
+    "생식기",
+    "자궁",
+    "고환",
+    "음부",
+    "정액",
+}
 
 
 class ProfanityWordListTest(unittest.TestCase):
@@ -12,7 +73,7 @@ class ProfanityWordListTest(unittest.TestCase):
         allow_words = importlib.import_module("allow_words")
         block_words = importlib.import_module("block_words")
 
-        self.assertIsInstance(allow_words.ALLOW_EXACT_WORDS, set)
+        self.assertEqual(EXPECTED_ALLOW_EXACT_WORDS, allow_words.ALLOW_EXACT_WORDS)
         self.assertEqual({"보지", "섹스"}, block_words.BLOCK_EXACT_WORDS)
         self.assertTrue(allow_words.ALLOW_EXACT_WORDS.isdisjoint(block_words.BLOCK_EXACT_WORDS))
 
